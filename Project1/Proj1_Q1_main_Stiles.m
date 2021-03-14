7867c9905a69f4ca60989073061a5e7b90fde5e7
syndr <syndr@DESKTOP-50GPNL6>
2021-03-14 19:41:47 -0400

    Plot approximations and L2 and H1 errors.

--- Project1/Proj1_Q1_main_Stiles.m
+++ Project1/Proj1_Q1_main_Stiles.m
@@ -10,7 +10,7 @@ function out = Proj1_Q1_main_Stiles(option)
     % Error tolerance object to hold error information for each refinement
     err = projectionError(option.refinements); 
     
-    %% Step 2: Build the mass matrix and the load vector and solve the linear system.
+    %% Step 2: Build the stiffness/mass matrices and load vector, then solve the linear system.
     
     % For every level of refinement
     for i = 1 : option.refinements        
@@ -36,12 +36,11 @@ function out = Proj1_Q1_main_Stiles(option)
         
         % Assemble the load vector b
         b = loadBuilder(mesh, option.pde.loadf);
-        % Force the Derichlet boundary conditions
+        % Force the Dirichlet boundary conditions
         b(1) = option.pde.leftBC;
         b(length(mesh)) = option.pde.rightBC;
         b(2:length(mesh)) = b(2:length(mesh)) - A(2:length(mesh), 1) * option.pde.leftBC;
         b(1:length(mesh)-1) = b(1:length(mesh)-1) - A(1:length(mesh)-1, length(mesh)) * option.pde.rightBC;
-        
         
         % Solve the linear system of equations 
         free_idx = 2 : (length(mesh) - 1);
@@ -54,14 +53,29 @@ function out = Proj1_Q1_main_Stiles(option)
         figure;
         plot(mesh, approxu, 'b.')
         hold on 
-        Pif = option.pde.exactu(mesh);
-        plot(mesh, Pif, 'r-')
+        exact = option.pde.exactu(mesh);
+        plot(mesh, exact, 'r-')
+        % Save figure as PNG file
+        filename = ['.\plots\refinement_', num2str(i), '.png'];
+        saveas(gcf, filename);
         
         % Compute L2 and H1 errors
         err.L2(i) = L2error(mesh, option.pde.exactu, approxu);
         err.H1(i) = H1error(mesh, option.pde.uprime, approxu);
     end
     
-    % TODO - Plot the L2 and H1 error
+    %% Step 3: Postprocessing -  Plot the L2 and H1 error
+    figure;
+    loglog(err.h, err.L2, 'o-r', ...
+           err.h, err.H1, 's-b', ... 
+           err.h(1:end), 5*err.h(1:end).^2, 'k-', ...
+           err.h(1:end), 10*err.h(1:end), 'g-');
+    lgd = legend('L2 error', 'H1 error', 'O(h^2)', 'O(h)');
+    lgd.Location = 'northwest';
+    xlabel('h');
+    ylabel('Errors');
+    % Save figure as PNG file
+    filename = '.\plots\errors.png';
+    saveas(gcf, filename);
 end
 
